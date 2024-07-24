711c50c02dcc66bebd8b095a461b3021d93f6c0a
Kevin <kevhoang97@gmail.com>
2024-07-23 20:09:50 -0400

    added blank handlebars pages

---
+++ public/js/profile.js
@@ -0,0 +1,47 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const name = document.querySelector('#blog-name').value.trim();
+    const body = document.querySelector('#blog-body').value.trim();
+  
+    if (name && body) {
+      const response = await fetch(`/api/blogs`, {
+        method: 'POST',
+        body: JSON.stringify({ name, body }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Failed to create blogpost');
+      }
+    }
+  };
+  
+  const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+  
+      const response = await fetch(`/api/blogs/${id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Failed to delete blogpost');
+      }
+    }
+  };
+  
+  document
+    .querySelector('.new-blog-form')
+    .addEventListener('submit', newFormHandler);
+  
+  document
+    .querySelector('.blog-list')
+    .addEventListener('click', delButtonHandler);
+  
