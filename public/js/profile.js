ac4fc7604eef35b177a38c6d043707dcca52cea7
Kevin <kevhoang97@gmail.com>
2024-07-24 21:20:42 -0400

    adding edit option in

--- public/js/profile.js
+++ public/js/profile.js
@@ -37,6 +37,21 @@ const delButtonHandler = async (event) => {
   }
 };
 
+const editButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+    const response = await fetch(`/api/blogs/${id}`, {
+      method: 'PUT',
+    });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to delete project');
+    }
+  }
+};
+
 document
   .querySelector('.new-blog-form')
   .addEventListener('submit', newFormHandler);
